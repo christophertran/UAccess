22adff29f8cedaf409552754f7b5605098f20781
Christopher Tran <ctran457@gmail.com>
2020-01-26 02:28:23 -0600

    idk

--- routes/profile.js
+++ routes/profile.js
@@ -9,14 +9,7 @@ var mongoose = require('mongoose');
 
 /* GET profile. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-    res.render('profile', { title: 'UAccess' , 
-                            data: mongoose.model('establishments').find(function(err, establishments) {
-                                  res.send(establishments);})
-                                });
-=======
     res.render('profile', { title: 'UAccess - All Establishments' });
->>>>>>> e5688ada7813c5ed8c0acfc6553280c7deeb6d75
 });
 
 module.exports = router;
