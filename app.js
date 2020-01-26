669a181c3409643b1fd067e4df841a0b6ad629b1
Christopher Tran <ctran457@gmail.com>
2020-01-25 23:32:12 -0600

    removed users

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 var mongoos = require('mongoose');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var profileRouter = require('./routes/profile');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/profile', ProfileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
