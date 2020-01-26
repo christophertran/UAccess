b0212d4c5c393972eb7b0d3311ee24111abfd1f8
Rohit Barichello <rohitbarichello@gmail.com>
2020-01-25 23:50:21 -0600

    small changes

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/profile', ProfileRouter);
+app.use('/profile', profileRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
