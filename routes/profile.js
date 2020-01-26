67d0b04aa7a5893290c3cd7d91f436ffc8ea2202
Christopher Tran <ctran457@gmail.com>
2020-01-25 23:39:46 -0600

    edited profile.ejs profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET profile. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('profile', { title: 'UAccess' });
 });
 
 module.exports = router;
